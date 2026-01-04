3d54af28e8c2d607a9e8a5457842af6d53396e86
gossyozzy <gossyozzy@gmail.com>
2026-01-03 18:38:46 -0700

    added player object and ability to rotate

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -10,13 +11,22 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
